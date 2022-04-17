1c1cb8353784cc12d16d42b0990dbe9cfa8441c9
DragonslayerLB <97277758+DragonslayerLB@users.noreply.github.com>
2022-04-17 13:21:06 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.ArrayList;
+
 public class Duke {
+    // Region - Fields and Const
+    private static ArrayList<String> tasks = new ArrayList<>(Constants.MAX_TASK_CAPACITY);
+
+    static void processNextCommandFromUser() {
+        String userInput = Utility.getNextLineOfInput().trim();
+
+        // check for termination statement
+        if (userInput.equals(Commands.BYE)) {
+            System.out.println(Constants.GOOD_BYE_LINE);
+            return;
+        }
+
+        if (userInput.equals(Commands.LIST)) {
+            // process list command
+            Utility.printToConsoleEnumerated(tasks);
+        } else {
+            // echo command
+            tasks.add(userInput);
+            System.out.println("added: " + userInput + "\n");
+        }
+
+        processNextCommandFromUser();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Constants.GREETING_LINE);
+        processNextCommandFromUser();
     }
 }
